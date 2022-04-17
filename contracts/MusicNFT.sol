a12558be55ad0016932ab86ea74fc94c47bfb342
allegorywrite <arita.tomoki@icloud.com>
2022-04-17 19:09:38 +0900

    develop contracts

--- contracts/MusicNFT.sol
+++ contracts/MusicNFT.sol
@@ -11,15 +11,89 @@ contract MusicNFT is ERC1155, Ownable {
 
     string private _name;
     string private _symbol;
-
+    uint256 public totalSupply;
+    
+    //レアリティごとの供給量
+    mapping(uint256 => uint256) private _supplyOfEach;
+    //レアリティごとの最大供給量
+    mapping(uint256 => uint256) private _AMOUNT_OF_MAX_MINT;
+    //WhiteList用
     mapping(address => bool) private _isAuthenticated;
 
     constructor() ERC1155("ipfs://QmZUzZ88HX8USBkaGCowxVzasfi4StsEqs5TuxWYWciU7x/metadata/{id}.json") {
-        mint(1, 2);
+        // Normal A
+        _AMOUNT_OF_MAX_MINT[1] = 10;
+        // Normal B
+        _AMOUNT_OF_MAX_MINT[2] = 10;
+        // Normal C
+        _AMOUNT_OF_MAX_MINT[3] = 20;
+        // Normal D
+        _AMOUNT_OF_MAX_MINT[4] = 20;
+        // NTP Collab A
+        _AMOUNT_OF_MAX_MINT[5] = 1;
+        // NTP Collab B
+        _AMOUNT_OF_MAX_MINT[6] = 4;
+        // NTP Collab C
+        _AMOUNT_OF_MAX_MINT[7] = 1;
+        // NTP Collab D
+        _AMOUNT_OF_MAX_MINT[8] = 4;
+        // Cool Rulers Collab A
+        _AMOUNT_OF_MAX_MINT[9] = 1;
+        // Cool Rulers Collab B
+        _AMOUNT_OF_MAX_MINT[10] = 2;
+        // Cool Rulers Collab C
+        _AMOUNT_OF_MAX_MINT[11] = 2;
+        // Cool Rulers Collab D
+        _AMOUNT_OF_MAX_MINT[12] = 1;
+        // Cool Rulers Collab E
+        _AMOUNT_OF_MAX_MINT[13] = 2;
+        // Cool Rulers Collab F
+        _AMOUNT_OF_MAX_MINT[14] = 2;
+        // Acappella A
+        _AMOUNT_OF_MAX_MINT[15] = 5;
+        // Acappella B
+        _AMOUNT_OF_MAX_MINT[16] = 5;
+        // Instrumental A
+        _AMOUNT_OF_MAX_MINT[17] = 5;
+        // Instrumental A
+        _AMOUNT_OF_MAX_MINT[18] = 5;
     }
 
-    function mint(uint256 _tokenId, uint256 amount) public onlyOwner {
-        _mint(msg.sender, _tokenId, amount, "");
+    modifier supplyCheck(
+        uint256 _tokenId,
+        uint256 _amount
+    ){
+        require(_supplyOfEach[_tokenId] + _amount <= _AMOUNT_OF_MAX_MINT[_tokenId]);
+        _;
+    }
+
+    modifier supplyCheckBatch(
+        uint256[] memory _tokenIds,
+        uint256[] memory _amounts
+    ){
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            require(_supplyOfEach[_tokenIds[i]] + _amounts[i] <= _AMOUNT_OF_MAX_MINT[_tokenIds[i]]);
+        }
+        _;
+    }
+
+    function mint(uint256 _tokenId, uint256 _amount) public onlyOwner supplyCheck(_tokenId, _amount){
+        _supplyOfEach[_tokenId] += _amount;
+        totalSupply += _amount;
+
+        _mint(_msgSender(), _tokenId, _amount, "");
+    }
+    
+    function mintBatch(
+        uint256[] memory _tokenIds,
+        uint256[] memory _amounts
+    ) public onlyOwner supplyCheckBatch(_tokenIds, _amounts){
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            _supplyOfEach[_tokenIds[i]] += _amounts[i];
+            totalSupply += _amounts[i];
+        }
+
+        _mintBatch(_msgSender(), _tokenIds, _amounts, "");
     }
 
     function _beforeTokenTransfer(
@@ -29,10 +103,12 @@ contract MusicNFT is ERC1155, Ownable {
         uint256[] memory ids,
         uint256[] memory amounts,
         bytes memory data
-    ) internal override view {
+    ) internal override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
         if (from == address(0)) { return; }
         require(_isAuthenticated[to], "This address is not authenticated");
-        require(balanceOf(_msgSender(), ids[0]) <= 1);
+        require(balanceOf(_msgSender(), ids[0]) <= 1, "Max supply reached");
     }
 
     function name() public view virtual returns (string memory) {
